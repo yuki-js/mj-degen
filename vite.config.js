956c067821b4d678d818ac1340a0db5642703735
yuki-js <me@aoki.app>
2025-07-06 12:45:55 +0900

    feat: Add GitHub Actions workflow for deploying to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/mj-degen/",
   server: {
     host: "0.0.0.0",
   },
